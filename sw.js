4d88ebccf00ccb298c299756f458eb58c12d5014
bffx-updates <bffx@bfmidi.com>
2026-04-03 18:22:10 -0300

    deploy v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bfmidi-wifi-v26";
+const CACHE_NAME = "bfmidi-wifi-v27";
 
 // Core assets that MUST cache for the PWA install prompt.
 // Keep this list minimal and only with internal files to ensure install success.
